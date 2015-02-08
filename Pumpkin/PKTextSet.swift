540878113a656c98401815e92c4661671ef3d9a9
dimbow <monobeat.030@gmail.com>
2015-02-08 17:57:05 +0900

    checkMultiLineText : 変数宣言修正

--- Pumpkin/PKTextSet.swift
+++ Pumpkin/PKTextSet.swift
@@ -22,7 +22,7 @@ extension PKTextSet{
         let sepcnt = checkString.utf16Count; // 識別文字の文字数カウント ("/"や"\n"を考慮して)
         var isMultiLine:Bool = false; // マルチラインか否か？
         var txt = PKtext as String; // 編集済文字列用
-        var SerifMultiLine:[String] = []; // 複数行用配列
+        var MultiLine:[String] = []; // 複数行用配列
         
         whl:while(true){
             var chktxt = txt as NSString; // NSRange用
@@ -31,23 +31,23 @@ extension PKTextSet{
                 // 文字列抽出
                 isMultiLine = true;
                 var pos = advance(txt.startIndex, chk.location);
-                SerifMultiLine.append(txt.substringToIndex(pos));
+                MultiLine.append(txt.substringToIndex(pos));
                 
                 // 抽出した分+識別文字までを削除
                 pos = advance(txt.startIndex,chk.location+sepcnt);
                 txt.removeRange(txt.startIndex..<pos);
             }else{
                 if(isMultiLine == true){
-                    SerifMultiLine.append(chktxt);
+                    MultiLine.append(chktxt);
                     isMultiLine = false;
                 }
                 break whl;
             }
         }
         
-        if(SerifMultiLine.count != 0){
+        if(MultiLine.count != 0){
             isMultiLine = true;
         }
-        return (isMultiLine,SerifMultiLine);
+        return (isMultiLine,MultiLine);
     }
 }
