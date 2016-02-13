90cf79d11568b1f1c3ae671ffe2778ed3bc30ef6
Subaco Akimoto <monobeat.030@gmail.com>
2016-02-13 21:51:34 +0900

    Separate function: deviceDetect()

--- Pumpkin/Pumpkin.swift
+++ Pumpkin/Pumpkin.swift
@@ -8,8 +8,17 @@
 
 import Foundation
 
+typealias detectDeviceTuple = (
+  Type: String,
+  Name: String,
+  Size: (
+    Width: CGFloat,
+    Height: CGFloat
+  )
+)
+
 public class Pumpkin {
-  
+
   public init() {
     
   }
@@ -26,10 +35,26 @@ public class Pumpkin {
     return -1;
   }
   
-  public func detectDevice() -> String {
+  public func detectDeviceType() -> String {
+    return detectDevice().Type;
+  }
+  
+  public func detectDeviceName() -> String {
+    return detectDevice().Name;
+  }
+  
+  public func detectDeviceSize() -> (Height:CGFloat, Width: CGFloat) {
+    return (
+      Height: detectDevice().Size.Height,
+      Width : detectDevice().Size.Width
+    );
+  }
+  
+  
+  private func detectDevice() -> detectDeviceTuple {
     let size = UIScreen.mainScreen().bounds.size;
     var base_size:CGFloat = 0;
-
+    
     // Always detect device on "width size".
     if (detectOrientation() == 0) {
       // Portrait
@@ -38,26 +63,83 @@ public class Pumpkin {
       // Landscape
       base_size = size.height;
     } else {
-      return "Cannot detect device.";
+      return (
+        Type: "Unknown type.",
+        Name: "Cannot detect device.",
+        Size: (
+          Height : 0,
+          Width: 0
+        )
+      );
     }
     
     switch base_size {
     case DeviceSizeWidth.less_iPhone5.rawValue:
       if (size.height == DeviceSizeHeight.iPhone4.rawValue) {
-        return DeviceName.iPhone4.rawValue;
+        return (
+          Type: DeviceType.iPhone.rawValue,
+          Name: DeviceName.iPhone4.rawValue,
+          Size: (
+            Height: DeviceSizeHeight.iPhone4.rawValue,
+            Width : DeviceSizeWidth.less_iPhone5.rawValue
+          )
+        )
       }
-      return DeviceName.iPhone5.rawValue;
+
+      return (
+        Type: DeviceType.iPhone.rawValue,
+        Name: DeviceName.iPhone5.rawValue,
+        Size: (
+          Height: DeviceSizeHeight.iPhone5.rawValue,
+          Width : DeviceSizeWidth.less_iPhone5.rawValue
+        )
+      )
     case DeviceSizeWidth.iPhone6.rawValue:
-      return DeviceName.iPhone6.rawValue;
+      return (
+        Type: DeviceType.iPhone.rawValue,
+        Name: DeviceName.iPhone6.rawValue,
+        Size: (
+          Height: DeviceSizeHeight.iPhone6.rawValue,
+          Width : DeviceSizeWidth.iPhone6.rawValue
+        )
+      )
     case DeviceSizeWidth.iPhone6Plus.rawValue:
-      return DeviceName.iPhone6Plus.rawValue;
+      return (
+        Type: DeviceType.iPhone.rawValue,
+        Name: DeviceName.iPhone6Plus.rawValue,
+        Size: (
+          Height: DeviceSizeHeight.iPhone6Plus.rawValue,
+          Width : DeviceSizeWidth.iPhone6Plus.rawValue
+        )
+      )
     case DeviceSizeWidth.iPad.rawValue:
-      return DeviceName.iPad.rawValue;
+      return (
+        Type: DeviceType.iPad.rawValue,
+        Name: DeviceName.iPad.rawValue,
+        Size: (
+          Height: DeviceSizeHeight.iPad.rawValue,
+          Width : DeviceSizeWidth.iPad.rawValue
+        )
+      )
     case DeviceSizeWidth.iPadPro.rawValue:
-      return DeviceName.iPadPro.rawValue;
+      return (
+        Type: DeviceType.iPad.rawValue,
+        Name: DeviceName.iPadPro.rawValue,
+        Size: (
+          Height: DeviceSizeHeight.iPadPro.rawValue,
+          Width : DeviceSizeWidth.iPadPro.rawValue
+        )
+      )
       
     default:
-      return "Unknown device";
+      return (
+        Type: "Unknown type.",
+        Name: "Unknown device.",
+        Size: (
+          Height: 0,
+          Width : 0
+        )
+      )
     }
   }
 }
