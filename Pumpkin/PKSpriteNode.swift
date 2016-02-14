0e280ff1614b924df0994fd33c9d8a9e05899473
Subaco Akimoto <monobeat.030@gmail.com>
2016-02-14 16:53:04 +0900

    Switch node scale on iPhone/iPad

--- Pumpkin/PKSpriteNode.swift
+++ Pumpkin/PKSpriteNode.swift
@@ -9,14 +9,15 @@
 import SpriteKit
 
 public class PKSpriteNode {
-  
   private var texture:SKTexture!;
+  private var node:SKSpriteNode!;
   private var anchor:Dictionary<String, CGFloat> = ["x":0, "y":0];
   private var position:Dictionary<String, CGFloat> = ["x":0 ,"y":0 ,"z":0];
-  private var scale:CGFloat!;
   private var alpha:CGFloat = 1;
   private var name:String? = nil;
   
+  public var scale_on_iPhone:CGFloat!;
+  public var scale_on_iPad:CGFloat!;
   
   public init(texture: SKTexture, anchor:CGPoint = CGPointMake(0, 0) , scale: CGFloat = 1, position: CGPoint, position_z:CGFloat = 0, alpha:CGFloat = 1, name: String? = nil) {
     self.texture = texture;
@@ -29,8 +30,7 @@ public class PKSpriteNode {
     if let ay:CGFloat = anchor.y {
       self.anchor["y"] = ay;
     }
-    
-    
+
     // check position numeric.
     if let posx:CGFloat = position.x {
       self.position["x"] = posx;
@@ -41,7 +41,8 @@ public class PKSpriteNode {
     }
     
     self.position["z"] = position_z;
-    self.scale = scale;
+    self.scale_on_iPhone = scale;
+    self.scale_on_iPad = scale;
     self.alpha = alpha;
     
     if let n = name {
@@ -50,14 +51,20 @@ public class PKSpriteNode {
   }
   
   public func create() -> SKSpriteNode {
-    let node = SKSpriteNode(texture: self.texture);
+    self.node = SKSpriteNode(texture: self.texture);
     node.anchorPoint = CGPoint(x: self.anchor["x"]!, y: self.anchor["y"]!);
     node.position = CGPoint(x: self.position["x"]!, y: self.position["y"]!);
     node.zPosition = self.position["z"]!;
     node.alpha = self.alpha;
-    node.setScale(self.scale);
     node.name = self.name!;
+
+    if (Pumpkin().detectDeviceType() == Pumpkin.DeviceType.iPhone.rawValue) {
+      self.node.setScale(self.scale_on_iPhone);
+    } else if (Pumpkin().detectDeviceType() == Pumpkin.DeviceType.iPad.rawValue
+      || Pumpkin().detectDeviceType() == Pumpkin.DeviceType.iPadPro.rawValue) {
+      self.node.setScale(self.scale_on_iPad);
+    }
     
-    return node;
+    return self.node;
   }
 }
